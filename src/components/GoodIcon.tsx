6ce019b94b7fc01334fbed0cf2f860c2e1e51ef6
wongcmichelle <wong.cmichelle@gmail.com>
2021-03-15 21:15:51 -0400

    Update svgs

--- src/components/GoodIcon.tsx
+++ src/components/GoodIcon.tsx
@@ -14,20 +14,15 @@ const GoodIcon = (props: GoodIconProps) => (
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle
-        cx="16"
-        cy="16"
-        r="14.6854"
-        fill={props.colour || "#37D7AB"}
-        stroke={props.colour || "#37D7AB"}
-        strokeWidth="2.62921"
-      />
+      <circle cx="16" cy="16" r="16" fill="#37D7AB" />
       <path
         d="M24.32 16.64C24.32 20.8815 20.595 24.32 16 24.32C11.405 24.32 7.68 20.8815 7.68 16.64"
         stroke="white"
         strokeWidth="2.62921"
         strokeLinecap="square"
       />
+      <circle cx="10.995" cy="11.315" r="1.315" fill="white" />
+      <circle cx="20.995" cy="11.315" r="1.315" fill="white" />
     </svg>
   </SvgIcon>
 );
